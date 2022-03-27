f3ce158ae7826288223045bdc24bb79ee7dd04b1
Claudiu Alb <claudiu.bogdan.alb@gmail.com>
2022-03-28 00:44:42 +0300

    Add+delete functional

--- src/main/resources/static/pizzas.js
+++ src/main/resources/static/pizzas.js
@@ -1,34 +1,36 @@
 $(document).ready(() => {
-    $('#add-button-modal').click(() => {
-        const name = $('#modal-name').val();
-        const size = $('#modal-size').val();
-        const toppings = $('#modal-toppings').val();
+      $('#add-button-modal').click(() => {
+            console.log("am intrat");
+            const name = $('#modal-name').val();
+            const size = $('#modal-size').val();
+            const toppings = $('#modal-toppings').val();
 
-        const newPizza = {
-            name: name,
-            size: size,
-            toppings: toppings
-        };
+            const newPizza = {
+                name: name,
+                size: size,
+                toppings: [toppings]
+            };
 
-        fetch('api/pizzas', {
-            method: 'POST',
-            body: JSON.stringify(newPizza),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then(response => {
-            if (response.ok) {
-                location.reload();
-            } else {
-                alert("There are errors " + response.status);
-            }
+            fetch('api/pizzas', {
+                method: 'POST',
+                body: JSON.stringify(newPizza),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(response => {
+                if (response.ok) {
+                    location.reload();
+                } else {
+                    alert("There are errors " + response.status);
+                }
+            });
+
         });
-    });
-$('.delete-icon').click(function () {
-        const pizzaId = this.parentElement.id;
+         $('.delete-icon').click(function () {
+                const pizzaId = this.parentElement.id;
 
-        fetch('api/pizzas/' + pizzaId, {
-            method: 'DELETE'
-        }).then(response => location.reload());
-    });
+                fetch('api/pizzas/' + pizzaId, {
+                    method: 'DELETE'
+                }).then(response => location.reload());
+            });
 });
